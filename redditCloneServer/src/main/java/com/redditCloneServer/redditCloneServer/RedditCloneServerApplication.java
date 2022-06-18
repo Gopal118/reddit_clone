2f24cb50ef4504fe90f9547a90b63e773a5e32a9
Gopal <gopal.sabadotechnologies@gmail.com>
2022-06-18 17:40:01 +0530

    reddit server.

---
+++ redditCloneServer/src/main/java/com/redditCloneServer/redditCloneServer/RedditCloneServerApplication.java
@@ -0,0 +1,13 @@
+package com.redditCloneServer.redditCloneServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedditCloneServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedditCloneServerApplication.class, args);
+	}
+
+}
---
+++ redditCloneServer/src/test/java/com/redditCloneServer/redditCloneServer/RedditCloneServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redditCloneServer.redditCloneServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedditCloneServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
